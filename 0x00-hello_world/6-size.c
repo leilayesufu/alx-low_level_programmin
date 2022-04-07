f2ca13e9a0d86d1b266c0d15a9c8769b6e33c445
Leila Yesufu <78659709+leilayesufu@users.noreply.github.com>
2022-04-07 09:30:41 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: (1)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, ch, sizeof(ch));
-	return (1);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
 
 
